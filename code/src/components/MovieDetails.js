87012b77be1fa7a3853d67cf6b2ed026538a97e5
Kristin <kristin@iMac.local>
2022-10-14 14:36:53 +0200

    icons

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -44,11 +44,12 @@ const MovieDetails = () => {
         backgroundImage: `url(http://image.tmdb.org/t/p/original${details.backdrop_path})`
       }}>
       <div className="details-summary">
-        <div className="test">
-          <button type="button" onClick={goBack} className="link-back">
-            <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="black"><path className="back-button" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256S114.6 512 256 512s256-114.6 256-256zM116.7 244.7l112-112c4.6-4.6 11.5-5.9 17.4-3.5s9.9 8.3 9.9 14.8l0 64 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 64c0 6.5-3.9 12.3-9.9 14.8s-12.9 1.1-17.4-3.5l-112-112c-6.2-6.2-6.2-16.4 0-22.6z" /></svg> Movies
-          </button>
-        </div>
+        <button type="button" onClick={goBack} className="link-back">
+          <div className="test">
+            <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="blue"><path className="icon" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256S114.6 512 256 512s256-114.6 256-256zM116.7 244.7l112-112c4.6-4.6 11.5-5.9 17.4-3.5s9.9 8.3 9.9 14.8l0 64 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 64c0 6.5-3.9 12.3-9.9 14.8s-12.9 1.1-17.4-3.5l-112-112c-6.2-6.2-6.2-16.4 0-22.6z" /></svg>
+          </div>
+            Movie
+        </button>
         <img className="details-summary-poster" src={details ? `https://image.tmdb.org/t/p/w342${details.poster_path}` : ''} alt={details.title} />
         <div className="details-summary-text">
           <h1 className="details-summary-text-header">
